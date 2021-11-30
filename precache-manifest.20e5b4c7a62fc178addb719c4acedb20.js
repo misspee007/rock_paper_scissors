15d864ea3e3ea69b075e965f6fdff18dc597dc75
Precious Abubakar <misspee007@gmail.com>
2021-11-30 16:01:12 +0100

    Updates

---
+++ precache-manifest.20e5b4c7a62fc178addb719c4acedb20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd97029a34a257fba5ebde917735fae0",
+    "url": "/rock_paper_scissors/index.html"
+  },
+  {
+    "revision": "326ea521cea06ffe6fd7",
+    "url": "/rock_paper_scissors/static/css/main.34bce201.chunk.css"
+  },
+  {
+    "revision": "38b63845285aa863597e",
+    "url": "/rock_paper_scissors/static/js/2.b95c2202.chunk.js"
+  },
+  {
+    "revision": "3405545c67c2b04913629256d4fb5d95",
+    "url": "/rock_paper_scissors/static/js/2.b95c2202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326ea521cea06ffe6fd7",
+    "url": "/rock_paper_scissors/static/js/main.e14abaee.chunk.js"
+  },
+  {
+    "revision": "97112fb09606a03af2bb",
+    "url": "/rock_paper_scissors/static/js/runtime-main.bff54731.js"
+  }
+]);
